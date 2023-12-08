1e6476f221b7c536ac4c724301e1ecd0857a9231
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2023-12-08 11:40:14 +0100

    Delete WebTech1_feleves_beadando/index.js

--- WebTech1_feleves_beadando/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const animatedElements = document.querySelectorAll('.animate-up');
-
-    setTimeout(function () {
-        animatedElements.forEach(function (element) {
-            element.classList.add('animate-up');
-        });
-    }, 500);
-});
