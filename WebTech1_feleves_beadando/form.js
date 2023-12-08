a0a0f64f2d0d4427062b6825e0bb7c380d0c3f8a
urbanoliver <90559472+urbanoliver@users.noreply.github.com>
2023-12-08 11:40:06 +0100

    Delete WebTech1_feleves_beadando/form.js

--- WebTech1_feleves_beadando/form.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.querySelector("#contact form");
-
-    contactForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        alert("Köszönjük!");
-    });
-});
